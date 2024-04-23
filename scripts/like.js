4f84ac1272a983deff3135680a150f85ffae3aa2
Morrriarty <159139396+Morrriarty@users.noreply.github.com>
2024-04-23 12:11:59 +0500

    1 itter

--- scripts/like.js
+++ scripts/like.js
@@ -1,13 +1,3 @@
-/* этот скрипт использует такие имена классов:
-✦ like-icon — для svg-иконки анимированного сердца
-✦ card__like-button — для кнопки Like рядом с иконкой
-✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ is-liked — для обозначения состояния лайкнутой иконки в виде сердца
-✦ button__text — для обозначения текстового элемента внутри кнопки
-Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
-*/
-
 const likeHeartArray = document.querySelectorAll('.like-icon');
 const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
@@ -29,12 +19,12 @@ function toggleIsLiked(heart, button) {
 function setButtonText(heart, button) {
   if ([...heart.classList].includes('is-liked')) {
     setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Unlike'),
+      () => (button.querySelector('.button_text').textContent = 'Unlike'),
       500
     );
   } else {
     setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Like'),
+      () => (button.querySelector('.button_text').textContent = 'Like'),
       500
     );
   }
